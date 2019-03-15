d31fb79449bfa31c13cd9e35e67c5442368db8e0
Walid Al-Saqaf <wsaqaf@gmail.com>
2019-03-15 11:11:30 +0100

    Various improvements and updates

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190315092312) do
+ActiveRecord::Schema.define(version: 20190315094247) do
 
   create_table "claim_reviews", force: :cascade do |t|
     t.string "src_id"
