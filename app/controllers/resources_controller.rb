ea3d23106937af05ed84e5ed2827babe794f1e1e
Walid Al-Saqaf <wsaqaf@gmail.com>
2019-03-27 08:29:58 +0100

    Various improvements and updates

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -10,11 +10,11 @@ class ResourcesController < ApplicationController
       elsif (params[:q].present?)
         tmp=Resource.order(:name).where("name like '%"+params[:q]+"%'")
         @total_count=tmp.count
-        @pagy, @resources = pagy(tmp, items: 10)
+        @pagy, @resources = pagy(tmp, items: 50)
       else
         tmp=Resource.all.order("created_at DESC")
         @total_count=tmp.count
-        @pagy, @resources = pagy(tmp, items: 10)
+        @pagy, @resources = pagy(tmp, items: 50)
       end
     end
 
