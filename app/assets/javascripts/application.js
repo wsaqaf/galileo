73b1609988bbfb26a5bcae9715b27ea55e6c72fa
Walid Al-Saqaf <wsaqaf@gmail.com>
2019-04-08 09:51:12 +0200

    Various improvements and updates

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -107,3 +107,12 @@ function URLPreview(s)
       }
     });
  }
+
+ function ResponsiveMenu() {
+   var x = document.getElementById("myTopnav");
+   if (x.className === "topnav") {
+     x.className += " responsive";
+   } else {
+     x.className = "topnav";
+   }
+ }
