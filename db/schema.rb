d9fbb03be982ddfe57939aa752e3af6190f28288
Walid Al-Saqaf <wsaqaf@gmail.com>
2019-05-24 11:08:28 +0200

    Various improvements and updates

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,6 @@
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
 # It's strongly recommended that you check this file into your version control system.
-
 ActiveRecord::Schema.define(version: 20190521085720) do
 
   create_table "claim_reviews", force: :cascade do |t|
