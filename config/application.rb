2d090d95f661c1eb4dd3ace4784c8de9e1118bf2
Walid Al-Saqaf <wsaqaf@gmail.com>
2019-06-09 20:28:17 +0200

    Various improvements and updates

--- config/application.rb
+++ config/application.rb
@@ -19,7 +19,6 @@ config.before_configuration do
   env_file = File.join(Rails.root, 'config', 'local_env.yml')
   YAML.load(File.open(env_file)).each do |key, value|
     ENV[key.to_s] = value
-puts("\n\n\nENV["+key.to_s+"] is "+value.to_s+"\n\n\n")
   end if File.exists?(env_file)
 end
     # Settings in config/environments/* take precedence over those specified here.
