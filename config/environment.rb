48a848b3c003999f9f9b484891856cd6ac56e7eb
Walid Al-Saqaf <wsaqaf@gmail.com>
2019-07-31 18:38:21 +0200

    Various improvements and updates

--- config/environment.rb
+++ config/environment.rb
@@ -4,6 +4,8 @@ require_relative 'application'
 # Initialize the Rails application.
 Rails.application.initialize!
 
+Encoding.default_internal, Encoding.default_external = ['utf-8'] * 2
+
 ActionMailer::Base.smtp_settings = {
 :address => ENV["SMTP_HOST"],
 :port => ENV["SMTP_PORT"],
